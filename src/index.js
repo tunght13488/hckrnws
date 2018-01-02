8d6c176bdd21b894a2697818d839b065f34ae25f
Ha The Tung <tunght13488@gmail.com>
2018-01-03 07:42:41 +0800

    wip

--- src/index.js
+++ src/index.js
@@ -1,16 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { browserHistory, Route, Router } from "react-router";
+import { routerReducer, syncHistoryWithStore } from "react-router-redux";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
 import App from "./App";
 import "./index.css";
+import rootReducer from "./reducers";
 import registerServiceWorker from "./registerServiceWorker";
-import configureStore from "./store";
 
-let store = configureStore();
+const middleware = [
+  thunkMiddleware,
+  createLogger(),
+];
+
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+/* eslint-enable */
+
+
+const reducer = combineReducers({
+  ...rootReducer,
+  routing: routerReducer,
+});
+
+debugger;
+
+const store = createStore(
+  reducer,
+  composeEnhancers(
+    applyMiddleware(...middleware),
+  ),
+);
+
+const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App/>
+    <Router history={history}>
+      <Route exact path="/" component={App}/>
+    </Router>
   </Provider>,
   document.getElementById("root"),
 );
